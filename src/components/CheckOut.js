d9fddc51c5fd7a71c169953fc84a050e83ff6d5c
hamadou diallo <hamadou_diallo@yahoo.com>
2019-07-10 21:01:48 -0400

    final touch css2

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -3,10 +3,10 @@ import React from "react";
 const CheckOut = ({ total }) => {
   let isEnabled = total && total > 0;
   let styling = total > 0 ? "btn_active" : "btn";
-
+  let checkOutStyle = total > 0 ? "checkout-btn" : "checkout-inactive";
   return (
     <div className={`container ${styling}`}>
-      <button disabled={!isEnabled} className="checkout-btn">
+      <button disabled={!isEnabled} className={checkOutStyle}>
         Proceed to Checkout
       </button>
     </div>
